107232ac335f7a986deb9df35f0c7ac474e6b4ee
Viet Khanh <vietkhanh93@gmail.com>
2024-09-06 00:28:27 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,16 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const hodanit1 = 24;
+    const hoidanit = {
+        name: "hoidanit",
+        tuoi: 34
+    }
+    const hoidanit2 = [1, 2, 3, 4]
     return (
         <>
-            <div>Ho viet khanh</div>
+            <div>{JSON.stringify(hoidanit)}Ho viet khanh</div>
+            <div>{hoidanit2[2]}Ho viet khanh</div>
             <div className="child">child</div>
         </>
 
